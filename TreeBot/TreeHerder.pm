1d1a68582f6e6f0d440e9e2d2eb9633538b9b134
Ed Morley <emorley@mozilla.com>
2015-12-03 16:33:50 +0000

    Append forward slash to Treeherder API URLs to avoid 301 redirects
    This fixes the TreeBot occurrences in:
    https://bugzilla.mozilla.org/show_bug.cgi?id=1230179

--- TreeBot/TreeHerder.pm
+++ TreeBot/TreeHerder.pm
@@ -32,10 +32,10 @@ sub poll {
     $self->resultset(undef);
 
     # grab resultset
-    print "requesting https://treeherder.mozilla.org/api/project/bmo-master/resultset\n";
+    print "requesting https://treeherder.mozilla.org/api/project/bmo-master/resultset/\n";
     $kernel->post(
         'ua', 'request', 'response',
-        HTTP::Request->new( GET => 'https://treeherder.mozilla.org/api/project/bmo-master/resultset' ),
+        HTTP::Request->new( GET => 'https://treeherder.mozilla.org/api/project/bmo-master/resultset/' ),
         'resultset'
     );
 
@@ -76,10 +76,10 @@ sub resultset_handler {
                 $self->_touch($id);
             }
             else {
-                print "requesting https://treeherder.mozilla.org/api/project/bmo-master/resultset/$id/status\n";
+                print "requesting https://treeherder.mozilla.org/api/project/bmo-master/resultset/$id/status/\n";
                 $kernel->post(
                     'ua', 'request', 'response',
-                    HTTP::Request->new( GET => "https://treeherder.mozilla.org/api/project/bmo-master/resultset/$id/status" ),
+                    HTTP::Request->new( GET => "https://treeherder.mozilla.org/api/project/bmo-master/resultset/$id/status/" ),
                     "status.$id"
                 );
             }
